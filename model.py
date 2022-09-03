7c9ce19c112446ef29a6995d05e4d554d70b8601
danalee <eleeje97@gmail.com>
2022-09-03 11:55:04 +0900

    add model: MyCloth

--- model.py
+++ model.py
@@ -15,4 +15,20 @@ class User(db.Model):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
-        return f'[USER] id: {self.id}, nickname: {self.nickname}, gender: {self.gender}, full_body_img_path: {self.full_body_img_path}'
+        return self.as_dict()
+
+
+class MyCloth(db.Model):
+    __tablename__ = 'MY_CLOTH'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String(50), db.ForeignKey('USER.id'), nullable=False)
+    origin_img_path = db.Column(db.String(255), nullable=True)
+    season = db.Column(db.String(30), nullable=True)
+    is_user_img = db.Column(db.Boolean, nullable=True)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+    def __repr__(self):
+        return self.as_dict()
