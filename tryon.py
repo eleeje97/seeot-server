23f2091b5beacd9c0ebd65a2342e28e2aa0f8f3e
danalee <eleeje97@gmail.com>
2022-09-16 10:02:48 +0000

    update .gitignore

--- tryon.py
+++ tryon.py
@@ -5,7 +5,10 @@ tryon = Blueprint('tryon', __name__, url_prefix='/tryon')
 
 @tryon.route('/', methods=['GET'])
 def try_on():
+    print('Try On START!!!')
+
     from models.dior import run
+
     user_id = request.args['user_id']
     top = request.args['top']
     bottom = request.args['bottom']
@@ -13,3 +16,6 @@ def try_on():
 
     # 문서에 저장
     run.try_on(0, 1, 2, 1)
+
+    return {'message': 'SUCCESS!'}
+
