a099eaca616ca9fd83bfa467f4063c020d8bd25f
danalee <eleeje97@gmail.com>
2022-09-19 08:27:51 +0000

    human_parsing/run.py

---
+++ models/human_parsing/run.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+
+def human_parsing(origin_img_path, output_img_path):
+    # copy and put image into inputs/
+    shutil.copy(origin_img_path, '/data/seeot-server/models/human_parsing/inputs/image.jpg')
+
+    # run image parsing
+    os.system("python models/human_parsing/simple_extractor.py --dataset 'lip' --model-restore 'checkpoints/final.pth' --input-dir 'inputs' --output-dir 'outputs'")
+
+    # copy and put output image into /data/seeot-data/
+    if os.path.exists('/data/seeot-server/models/human_parsing/outputs/image.png'):
+        shutil.copy('/data/seeot-server/models/human_parsing/outputs/image.png', output_img_path)
+
+        # remove images in inputs/
+        os.remove('/data/seeot-server/models/human_parsing/inputs/image.jpg')
+
+        # remove images in outputs/
+        os.remove('/data/seeot-server/models/human_parsing/outputs/image.png')
+
+
+
+# print(os.environ.get('CUDA_PATH'))
+
+# import torch
+# print(torch.cuda.is_available())
+
+origin_img_path = '/data/seeot-model/02_4_full.jpg'
+output_img_path = '/data/seeot-model/temp/02_4_full.png'
+human_parsing(origin_img_path, output_img_path)
